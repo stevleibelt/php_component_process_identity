e08069e24189c48d72d8002dbc36258737d5e3ce
stevleibelt <stev@leibelt.de>
2013-07-14 23:37:59 +0200

    Finished classnameAndTimetstampeIdentity

---
+++ source/Net/Bazzline/Component/ProcessIdentity/ClassnameAndTimestampIdentity.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-11
+ */
+
+namespace Net\Bazzline\Component\ProcessIdentity;
+
+/**
+ * Class ClassnameAndTimestampIdentity
+ *
+ * @package Net\Bazzline\Component\ProcessIdentity
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-11
+ */
+class ClassnameAndTimestampIdentity implements IdentityInterface
+{
+    /**
+     * @var string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-11
+     */
+    protected $id;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getId()
+    {
+        if (is_null($this->id)) {
+            $this->id = get_class($this) . '_' . time();
+        }
+
+        return $this->id;
+    }
+}
