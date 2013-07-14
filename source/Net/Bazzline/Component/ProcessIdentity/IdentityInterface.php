a78ea331224f2a32e2a8ecea829cd9c6d9ceea51
stevleibelt <stev@leibelt.de>
2013-07-14 23:37:47 +0200

    Updated interface

--- source/Net/Bazzline/Component/ProcessIdentity/IdentityInterface.php
+++ source/Net/Bazzline/Component/ProcessIdentity/IdentityInterface.php
@@ -1,8 +1,24 @@
 <?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-11
+ */
 
 namespace Net\Bazzline\Component\ProcessIdentity;
 
-interface IndentityInterface
+/**
+ * Class IndentityInterface
+ *
+ * @package Net\Bazzline\Component\ProcessIdentity
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-11
+ */
+interface IdentityInterface
 {
+    /**
+     * @return string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-11
+     */
     public function getId();
 }
