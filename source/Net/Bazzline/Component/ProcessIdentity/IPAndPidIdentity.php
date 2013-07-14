9431129a5af920411dfc452aefdb148a0473e8e4
stevleibelt <stev@leibelt.de>
2013-07-14 23:38:13 +0200

    Finished IpAndPidIdentity

--- source/Net/Bazzline/Component/ProcessIdentity/IPAndPidIdentity.php
+++ source/Net/Bazzline/Component/ProcessIdentity/IPAndPidIdentity.php
@@ -1,6 +1,23 @@
+<?php
+/**
+* @author stev leibelt <artodeto@arcor.de>
+* @since 2013-07-11
+*/
 
-class IPAndPidIdentity implements IndentityInterface
+namespace Net\Bazzline\Component\ProcessIdentity;
+
+/**
+ * Class IPAndPidIdentity
+ *
+ * @package Net\Bazzline\Component\ProcessIdentity
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-11
+ */
+class IPAndPidIdentity implements IdentityInterface
 {
+    /**
+     * {@inheritDoc}
+     */
     public function getId()
     {
         return $_SERVER['SERVER_ADDR'] . '_' . getmypid();
