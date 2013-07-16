243e4c41879393b10773bc440026c0161e2c6ff0
stevleibelt <stev@leibelt.de>
2013-07-17 01:35:18 +0200

    Coverd IPAndPidIdentity with unittest

---
+++ test/Test/Net/Bazzline/Component/ProcessIdentity/IPAndPidIdentityTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-17 
+ */
+
+namespace Test\Net\Bazzline\Component\ProcessIdentity;
+
+use Net\Bazzline\Component\ProcessIdentity\IPAndPidIdentity;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class IPAndPidIdentity
+ *
+ * @package Test\Net\Bazzline\Component\ProcessIdentity
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-17
+ */
+class IPAndPidIdentityTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-17
+     */
+    protected function setUp()
+    {
+        $_SERVER['SERVER_ADDR'] = '123.456.789.012';
+    }
+
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-17
+     */
+    protected function tearDown()
+    {
+        unset($_SERVER['SERVER_ADDR']);
+    }
+
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-17
+     */
+    public function testGetId()
+    {
+        $identity = $this->getNewIdentity();
+        $expectedId = $_SERVER['SERVER_ADDR'] . '_' . getmypid();
+
+        $this->assertEquals($expectedId, $identity->getId());
+    }
+
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-17
+     */
+    public function testSetAndGetId()
+    {
+        $identity = $this->getNewIdentity();
+        $id = 'exampleId';
+
+        $this->assertEquals($identity, $identity->setId($id));
+        $this->assertEquals($id, $identity->getId());
+    }
+
+    /**
+     * @return IPAndPidIdentity
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-17
+     */
+    private function getNewIdentity()
+    {
+        return new IPAndPidIdentity();
+    }
+}
